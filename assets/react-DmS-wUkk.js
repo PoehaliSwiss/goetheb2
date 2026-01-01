9770faf6459113dd8832a0ad6239990f7cc6a47a
PoehaliSwiss <PoehaliSwiss@users.noreply.github.com>
2026-01-01 11:01:30 +0000

    deploy: 781a8c7dbb5b8499eede530ab189aee59868178f

--- assets/react-DmS-wUkk.js
+++ assets/react-DmS-wUkk.js
@@ -1,4 +1,4 @@
-import{R as W}from"./index-Z5VBIotF.js";const $=["abort","canplay","canplaythrough","durationchange","emptied","encrypted","ended","error","loadeddata","loadedmetadata","loadstart","pause","play","playing","progress","ratechange","seeked","seeking","stalled","suspend","timeupdate","volumechange","waiting","waitingforkey","resize","enterpictureinpicture","leavepictureinpicture","webkitbeginfullscreen","webkitendfullscreen","webkitpresentationmodechanged"],A=globalThis.document?.createElement("template");A&&(A.innerHTML=`
+import{R as W}from"./index-BF-ZUyPs.js";const $=["abort","canplay","canplaythrough","durationchange","emptied","encrypted","ended","error","loadeddata","loadedmetadata","loadstart","pause","play","playing","progress","ratechange","seeked","seeking","stalled","suspend","timeupdate","volumechange","waiting","waitingforkey","resize","enterpictureinpicture","leavepictureinpicture","webkitbeginfullscreen","webkitendfullscreen","webkitpresentationmodechanged"],A=globalThis.document?.createElement("template");A&&(A.innerHTML=`
     <style>
       :host {
         display: inline-block;
